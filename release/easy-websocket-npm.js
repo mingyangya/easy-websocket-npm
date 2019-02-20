9116cea1ccc412894e7eacc0b6bdfa20d2466d49
haomingyang <haomingyangchn@163.com>
2019-02-20 21:58:00 +0800

    版本v0.0.5
    1.连接失败的停止心跳检测

--- release/easy-websocket-npm.js
+++ release/easy-websocket-npm.js
@@ -90,12 +90,13 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                 this.ws.onmessage = function (evet) {
                     var pingData = void 0;
                     if (_this2.opt.serverType === "deamon") {
-                        pingData = evet.data.message;
+                        pingData = JSON.parse(evet.data).message;
                     } else {
                         pingData = evet.data;
                     }
-                    _this2.pingPong = 'pong'; // 服务器端返回pong,修改pingPong的状态
-
+                    if (pingData === "pong") {
+                        _this2.pingPong = 'pong'; // 服务器端返回pong,修改pingPong的状态
+                    }
                     return _this2.opt.msgCb(evet);
                 };
                 this.ws.onerror = function (e) {
@@ -149,6 +150,11 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                     if (this.opt.debug) {
                         console.log(this.name + "websocket\u624B\u52A8\u5173\u95ED");
                     }
+                    if (this.pingInterval !== undefined && this.pongInterval !== undefined) {
+                        // 清除定时器
+                        clearInterval(this.pingInterval);
+                        clearInterval(this.pongInterval);
+                    }
                 }
             }
         }, {
@@ -192,15 +198,221 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                 }, this.opt.pingTime);
 
                 this.pongInterval = setInterval(function () {
-                    // this.pingPong = false;
                     if (_this4.pingPong === 'ping') {
                         _this4.closeHandle('pingPong没有改变为pong'); // 没有返回pong 重启webSocket
+                    } else {
+                        if (_this4.opt.debug) {
+                            console.log('返回pong');
+                        }
+                        _this4.pingPong = 'ping'; // 重置为ping 若下一次 ping 发送失败 或者pong返回失败(pingPong不会改成pong)，将重启.
                     }
-                    // 重置为ping 若下一次 ping 发送失败 或者pong返回失败(pingPong不会改成pong)，将重启.
-                    if (_this4.opt.debug) {
-                        console.log('返回pong');
+                }, this.opt.pongTime);
+            }
+        }]);
+
+        return EasyWebSocket;
+    }();
+
+    return EasyWebSocket;
+});
+(function (global, factory) {
+    if ((typeof module === "undefined" ? "undefined" : _typeof(module)) === "object" && _typeof(module.exports) === "object") {
+        module.exports = factory(); // common.js
+    } else if (typeof define === 'function') {
+        define(function () {
+            return factory();
+        }); //amd规范 ,require.js
+    } else {
+        global.EasyWebSocket = factory(); //浏览器环境
+    }
+})(typeof window !== "undefined" ? window : global, function () {
+    'use strict';
+
+    var EasyWebSocket = function () {
+        /**
+         * @description: 初始化实例属性，保存参数
+         * @param {Object} opt 参数配置
+         * @param {String} opt.url ws的接口
+         * @param {Function} opt.msgCb 服务器信息的回调传数据给函数
+         * @param {String} [opt.name=default] 可选值 用于区分ws
+         * @param {Boolean} [opt.debug=false] 可选值 是否开启调试模式
+         * @param {Boolean} [opt.failNum=3] 可选值 连接失败后重连的次数
+         * @param {Number} [opt.delayConnectTime=3000] 可选值 重连的延时时间
+         * @param {String} [opt.cmd="ping"] 可选值 ping值
+         * @param {String} [opt.serverType="deamon"] 可选值 socket服务类型
+         * @param {Function} [opt.fail=null] 可选值 socket连接失败的回调
+         * @param {Function} [opt.success=null] 可选值 socket连接成功的回调
+         */
+        function EasyWebSocket(opt) {
+            var _this5 = this;
+
+            _classCallCheck(this, EasyWebSocket);
+
+            var defaultOpt = {
+                msgCb: function msgCb(evet) {
+                    if (_this5.opt.debug) {
+                        console.log('接收服务器消息的回调：', evet);
                     }
-                    _this4.pingPong = 'ping';
+                },
+                name: 'default',
+                debug: false,
+                failNum: 3,
+                delayConnectTime: 3000,
+                pingTime: 2000,
+                pongTime: 3000,
+                cmd: false,
+                serverType: "deamon",
+                fail: null,
+                success: null,
+                index: 0
+            };
+            this.opt = Object.assign({}, defaultOpt, opt);
+            this.ws = null; // websocket对象
+            this.connectNum = 0;
+            this.status = null;
+        }
+
+        _createClass(EasyWebSocket, [{
+            key: "connect",
+            value: function connect(data) {
+                var _this6 = this;
+
+                this.ws = new WebSocket(this.opt.url);
+                this.ws.onopen = function (e) {
+                    // 连接 websocket 成功
+                    _this6.status = 'open';
+                    if (_this6.opt.cmd) {
+                        //是否开启心跳检测
+                        _this6.heartCheck(_this6.opt.cmd);
+                    }
+                    if (_this6.opt.debug) {
+                        console.log(_this6.opt.name + "\u8FDE\u63A5\u6210\u529F", e);
+                    }
+                    //连接成功
+                    _this6.opt.success && _this6.opt.success(_this6.opt.index);
+
+                    if (data !== undefined) {
+                        _this6.sendMessage(data);
+                    }
+                };
+                this.ws.onmessage = function (evet) {
+                    var pingData = void 0;
+                    if (_this6.opt.serverType === "deamon") {
+                        pingData = JSON.parse(evet.data).message;
+                    } else {
+                        pingData = evet.data;
+                    }
+                    if (pingData === "pong") {
+                        _this6.pingPong = 'pong'; // 服务器端返回pong,修改pingPong的状态
+                    }
+                    return _this6.opt.msgCb(evet);
+                };
+                this.ws.onerror = function (e) {
+                    _this6.errorHandle(e);
+                };
+                this.ws.onclose = function (err) {
+                    _this6.closeHandle(err);
+                };
+            }
+        }, {
+            key: "sendMessage",
+            value: function sendMessage(data) {
+                if (this.opt.debug) {
+                    console.log(this.opt.name + "\u53D1\u9001\u6D88\u606F\u7ED9\u670D\u52A1\u5668:", data);
+                }
+                var _data = (typeof data === "undefined" ? "undefined" : _typeof(data)) === "object" ? JSON.stringify(data) : data;
+                if (this.ws.readyState === 1) {
+                    // 当为OPEN时
+                    this.ws.send(_data);
+                }
+            }
+        }, {
+            key: "closeHandle",
+            value: function closeHandle() {
+                var _this7 = this;
+
+                var e = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'err';
+
+                this.connectNum++;
+                if (this.opt.debug) {
+                    console.log(this.opt.name + "\u65AD\u5F00\uFF0C" + this.opt.delayConnectTime + "ms\u540E\u91CD\u8FDEwebsocket,\u5C1D\u8BD5\u8FDE\u63A5\u7B2C" + this.connectNum + "\u6B21\u3002", e);
+                }
+                if (this.status !== 'close') {
+                    if (this.connectNum < this.opt.failNum) {
+                        setTimeout(function () {
+                            if (_this7.pingInterval !== undefined && _this7.pongInterval !== undefined) {
+                                // 清除定时器
+                                clearInterval(_this7.pingInterval);
+                                clearInterval(_this7.pongInterval);
+                            }
+                            _this7.connect(); // 重连
+                        }, this.opt.delayConnectTime);
+                    } else {
+                        if (this.opt.debug) {
+                            console.log(this.opt.name + "\u65AD\u5F00\uFF0C\u91CD\u8FDEwebsocket\u5931\u8D25\uFF01", e);
+                        }
+                        this.opt.fail && this.opt.fail(this.opt.index);
+                    }
+                } else {
+                    if (this.opt.debug) {
+                        console.log(this.name + "websocket\u624B\u52A8\u5173\u95ED");
+                    }
+                    if (this.pingInterval !== undefined && this.pongInterval !== undefined) {
+                        // 清除定时器
+                        clearInterval(this.pingInterval);
+                        clearInterval(this.pongInterval);
+                    }
+                }
+            }
+        }, {
+            key: "errorHandle",
+            value: function errorHandle() {
+                var e = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'err';
+
+                // 错误处理
+                if (this.opt.debug) {
+                    console.log(this.opt.name + "\u8FDE\u63A5\u9519\u8BEF:", e);
+                }
+            }
+        }, {
+            key: "closeMyself",
+
+
+            // 手动关闭WebSocket
+            value: function closeMyself() {
+                if (this.opt.debug) {
+                    console.log("\u5173\u95ED" + this.name);
+                }
+
+                this.status = 'close';
+                return this.ws.close();
+            }
+        }, {
+            key: "heartCheck",
+            value: function heartCheck(cmd) {
+                var _this8 = this;
+
+                // 心跳机制的时间可以自己与后端约定
+                this.pingPong = 'ping'; // ws的心跳机制状态值
+                this.pingInterval = setInterval(function () {
+                    if (_this8.ws.readyState === 1) {
+                        // 检查ws为链接状态 才可发送
+                        if (typeof cmd !== "string") {
+                            cmd = JSON.stringify(cmd);
+                        }
+                        _this8.ws.send(cmd); // 客户端发送ping
+                    }
+                }, this.opt.pingTime);
+
+                this.pongInterval = setInterval(function () {
+                    if (_this8.pingPong === 'ping') {
+                        _this8.closeHandle('pingPong没有改变为pong'); // 没有返回pong 重启webSocket
+                    } else {
+                        if (_this8.opt.debug) {
+                            console.log('返回pong');
+                        }
+                        _this8.pingPong = 'ping'; // 重置为ping 若下一次 ping 发送失败 或者pong返回失败(pingPong不会改成pong)，将重启.
+                    }
                 }, this.opt.pongTime);
             }
         }]);
--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@
         constructor(opt) {
             const defaultOpt = {
                 msgCb: ((evet) => {
-                    if(this.opt.debug){
+                    if (this.opt.debug) {
                         console.log('接收服务器消息的回调：', evet);
                     }
                 }),
@@ -39,9 +39,9 @@
                 pongTime: 3000,
                 cmd: false,
                 serverType: "deamon",
-                fail:null,
-                success:null,
-                index:0
+                fail: null,
+                success: null,
+                index: 0
             };
             this.opt = Object.assign({}, defaultOpt, opt);
             this.ws = null; // websocket对象
@@ -56,7 +56,7 @@
 
                 this.status = 'open';
                 //连接成功
-                this.opt.success&&this.opt.success(this.opt.index);
+                this.opt.success && this.opt.success(this.opt.index);
 
                 if (this.opt.cmd) { //是否开启心跳检测
                     this.heartCheck(this.opt.cmd);
@@ -71,12 +71,13 @@
             this.ws.onmessage = (evet) => {
                 let pingData;
                 if (this.opt.serverType === "deamon") {
-                    pingData=evet.data.message;
+                    pingData = JSON.parse(evet.data).message;
                 } else {
-                    pingData=evet.data;
+                    pingData = evet.data;
                 }
-                this.pingPong = 'pong'; // 服务器端返回pong,修改pingPong的状态
-
+                if (pingData === "pong") {
+                    this.pingPong = 'pong'; // 服务器端返回pong,修改pingPong的状态
+                }
                 return this.opt.msgCb(evet);
             };
             this.ws.onerror = (e) => {
@@ -117,11 +118,16 @@
                     if (this.opt.debug) {
                         console.log(`${this.opt.name}断开，重连websocket失败！`, e);
                     }
-                    this.opt.fail&&this.opt.fail(this.opt.index);
+                    this.opt.fail && this.opt.fail(this.opt.index);
                 }
             } else {
                 if (this.opt.debug) {
                     console.log(`${this.name}websocket手动关闭`);
+                }
+                if (this.pingInterval !== undefined && this.pongInterval !== undefined) {
+                    // 清除定时器
+                    clearInterval(this.pingInterval);
+                    clearInterval(this.pongInterval);
                 }
             }
 
@@ -159,19 +165,195 @@
             }, this.opt.pingTime);
 
             this.pongInterval = setInterval(() => {
-                // this.pingPong = false;
                 if (this.pingPong === 'ping') {
                     this.closeHandle('pingPong没有改变为pong'); // 没有返回pong 重启webSocket
+                } else {
+                    if (this.opt.debug) {
+                        console.log('返回pong');
+                    }
+                    this.pingPong = 'ping';  // 重置为ping 若下一次 ping 发送失败 或者pong返回失败(pingPong不会改成pong)，将重启.
                 }
-                // 重置为ping 若下一次 ping 发送失败 或者pong返回失败(pingPong不会改成pong)，将重启.
-                if(this.opt.debug){
-                    console.log('返回pong');
-                }
-                this.pingPong = 'ping';
-
             }, this.opt.pongTime)
         }
 
+    }
+
+    return EasyWebSocket;
+
+});
+(function (global, factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        module.exports = factory(); // common.js
+    } else if (typeof define === 'function') {
+        define(() => factory()); //amd规范 ,require.js
+    } else {
+        global.EasyWebSocket = factory(); //浏览器环境
+    }
+})(typeof window !== "undefined" ? window : global, function () {
+    'use strict';
+
+    class EasyWebSocket {
+        /**
+         * @description: 初始化实例属性，保存参数
+         * @param {Object} opt 参数配置
+         * @param {String} opt.url ws的接口
+         * @param {Function} opt.msgCb 服务器信息的回调传数据给函数
+         * @param {String} [opt.name=default] 可选值 用于区分ws
+         * @param {Boolean} [opt.debug=false] 可选值 是否开启调试模式
+         * @param {Boolean} [opt.failNum=3] 可选值 连接失败后重连的次数
+         * @param {Number} [opt.delayConnectTime=3000] 可选值 重连的延时时间
+         * @param {String} [opt.cmd="ping"] 可选值 ping值
+         * @param {String} [opt.serverType="deamon"] 可选值 socket服务类型
+         * @param {Function} [opt.fail=null] 可选值 socket连接失败的回调
+         * @param {Function} [opt.success=null] 可选值 socket连接成功的回调
+         */
+        constructor(opt) {
+            const defaultOpt = {
+                msgCb: ((evet) => {
+                    if (this.opt.debug) {
+                        console.log('接收服务器消息的回调：', evet);
+                    }
+                }),
+                name: 'default',
+                debug: false,
+                failNum: 3,
+                delayConnectTime: 3000,
+                pingTime: 2000,
+                pongTime: 3000,
+                cmd: false,
+                serverType: "deamon",
+                fail: null,
+                success: null,
+                index: 0
+            };
+            this.opt = Object.assign({}, defaultOpt, opt);
+            this.ws = null; // websocket对象
+            this.connectNum = 0;
+            this.status = null;
+        }
+
+        connect(data) {
+            this.ws = new WebSocket(this.opt.url);
+            this.ws.onopen = (e) => {
+                // 连接 websocket 成功
+                this.status = 'open';
+                if (this.opt.cmd) { //是否开启心跳检测
+                    this.heartCheck(this.opt.cmd);
+                }
+                if (this.opt.debug) {
+                    console.log(`${this.opt.name}连接成功`, e);
+                }
+                //连接成功
+                this.opt.success && this.opt.success(this.opt.index);
+
+                if (data !== undefined) {
+                    this.sendMessage(data);
+                }
+            };
+            this.ws.onmessage = (evet) => {
+                let pingData;
+                if (this.opt.serverType === "deamon") {
+                    pingData = JSON.parse(evet.data).message;
+                } else {
+                    pingData = evet.data;
+                }
+                if (pingData === "pong") {
+                    this.pingPong = 'pong'; // 服务器端返回pong,修改pingPong的状态
+                }
+                return this.opt.msgCb(evet);
+            };
+            this.ws.onerror = (e) => {
+                this.errorHandle(e);
+            };
+            this.ws.onclose = (err) => {
+                this.closeHandle(err);
+            }
+        };
+
+        sendMessage(data) {
+            if (this.opt.debug) {
+                console.log(`${this.opt.name}发送消息给服务器:`, data);
+            }
+            const _data = typeof data === "object" ? JSON.stringify(data) : data;
+            if (this.ws.readyState === 1) { // 当为OPEN时
+                this.ws.send(_data);
+            }
+        };
+
+        closeHandle(e = 'err') {
+            this.connectNum++;
+            if (this.opt.debug) {
+                console.log(`${this.opt.name}断开，${this.opt.delayConnectTime}ms后重连websocket,尝试连接第${this.connectNum}次。`, e);
+            }
+            if (this.status !== 'close') {
+                if (this.connectNum < this.opt.failNum) {
+                    setTimeout(() => {
+                        if (this.pingInterval !== undefined && this.pongInterval !== undefined) {
+                            // 清除定时器
+                            clearInterval(this.pingInterval);
+                            clearInterval(this.pongInterval);
+                        }
+                        this.connect(); // 重连
+                    }, this.opt.delayConnectTime)
+                } else {
+                    if (this.opt.debug) {
+                        console.log(`${this.opt.name}断开，重连websocket失败！`, e);
+                    }
+                    this.opt.fail && this.opt.fail(this.opt.index);
+                }
+            } else {
+                if (this.opt.debug) {
+                    console.log(`${this.name}websocket手动关闭`);
+                }
+                if (this.pingInterval !== undefined && this.pongInterval !== undefined) {
+                    // 清除定时器
+                    clearInterval(this.pingInterval);
+                    clearInterval(this.pongInterval);
+                }
+            }
+        };
+
+        errorHandle(e = 'err') {
+            // 错误处理
+            if (this.opt.debug) {
+                console.log(`${this.opt.name}连接错误:`, e);
+            }
+        };
+
+        // 手动关闭WebSocket
+        closeMyself() {
+            if (this.opt.debug) {
+                console.log(`关闭${this.name}`);
+            }
+
+            this.status = 'close';
+            return this.ws.close();
+        }
+
+        heartCheck(cmd) {
+            // 心跳机制的时间可以自己与后端约定
+            this.pingPong = 'ping'; // ws的心跳机制状态值
+            this.pingInterval = setInterval(() => {
+                if (this.ws.readyState === 1) {
+                    // 检查ws为链接状态 才可发送
+                    if (typeof cmd !== "string") {
+                        cmd = JSON.stringify(cmd);
+                    }
+                    this.ws.send(cmd); // 客户端发送ping
+                }
+            }, this.opt.pingTime);
+
+            this.pongInterval = setInterval(() => {
+                if (this.pingPong === 'ping') {
+                    this.closeHandle('pingPong没有改变为pong'); // 没有返回pong 重启webSocket
+                } else {
+                    if (this.opt.debug) {
+                        console.log('返回pong');
+                    }
+                    this.pingPong = 'ping';  // 重置为ping 若下一次 ping 发送失败 或者pong返回失败(pingPong不会改成pong)，将重启.
+                }
+            }, this.opt.pongTime)
+        }
     }
 
     return EasyWebSocket;
